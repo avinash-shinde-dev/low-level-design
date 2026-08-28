af9150793cff33eb382486edd7ec72c3f5cc8358
avinash-shinde-dev <avinash.shinde.dev@gmail.com>
2026-08-28 15:55:25 +0530

    feat: Payment enum added

---
+++ src/main/java/com/shikavani/lld/ridesharing/enums/PaymentStatus.java
@@ -0,0 +1,6 @@
+package com.shikavani.lld.ridesharing.enums;
+
+public enum PaymentStatus {
+    SUCCESS,
+    FAILED
+}
---
+++ src/main/java/com/shikavani/lld/ridesharing/enums/PaymentStrategyType.java
@@ -0,0 +1,8 @@
+package com.shikavani.lld.ridesharing.enums;
+
+public enum PaymentStrategyType {
+    CASH,
+    CREDIT_CARD,
+    DEBIT_CARD,
+    UPI
+}
