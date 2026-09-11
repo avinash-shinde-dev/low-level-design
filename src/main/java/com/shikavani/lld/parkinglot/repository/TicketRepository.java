1f51bc1bcbc9ebcbcc1f814decd12fc6f79b44f2
avinash-shinde-dev <avinash.shinde.dev@gmail.com>
2026-09-11 22:07:26 +0530

    feat: code refactoring

--- src/main/java/com/shikavani/lld/parkinglot/repository/TicketRepository.java
+++ src/main/java/com/shikavani/lld/parkinglot/repository/TicketRepository.java
@@ -1,4 +1,27 @@
 package com.shikavani.lld.parkinglot.repository;
 
-public class TicketRespository {
+import com.shikavani.lld.parkinglot.model.Ticket;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class TicketRepository implements InMemoryRepository<String, Ticket> {
+    private final Map<String, Ticket> ticketConcurrentHashMap = new ConcurrentHashMap<>();
+
+    @Override
+    public Ticket save(Ticket ticket) {
+        ticketConcurrentHashMap.put(ticket.getTicketId(), ticket);
+        return ticket;
+    }
+
+    @Override
+    public Ticket findById(String ticketId) {
+        return ticketConcurrentHashMap.get(ticketId);
+    }
+
+    @Override
+    public List<Ticket> findAll() {
+        return ticketConcurrentHashMap.values().stream().toList();
+    }
 }
