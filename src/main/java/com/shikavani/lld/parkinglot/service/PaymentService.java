a2d9a5c10367437f52fdd511ff49f7ca1478b75c
avinash-shinde-dev <avinash.shinde.dev@gmail.com>
2026-09-11 22:02:58 +0530

    feat(service): add PaymentService wrapping payment strategy execution

--- src/main/java/com/shikavani/lld/parkinglot/service/PaymentService.java
+++ src/main/java/com/shikavani/lld/parkinglot/service/PaymentService.java
@@ -1,4 +1,21 @@
 package com.shikavani.lld.parkinglot.service;
 
+import com.shikavani.lld.parkinglot.enums.PaymentMode;
+import com.shikavani.lld.parkinglot.model.PaymentRequest;
+import com.shikavani.lld.parkinglot.model.PaymentResponse;
+import com.shikavani.lld.parkinglot.strategy.payment.PaymentStrategy;
+
 public class PaymentService {
+
+    private final PaymentStrategy paymentStrategy;
+
+    public PaymentService(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public PaymentResponse pay(PaymentRequest paymentRequest){
+        System.out.println("Starting payment processing..");
+        // perform business validation
+        return this.paymentStrategy.pay(paymentRequest);
+    }
 }
