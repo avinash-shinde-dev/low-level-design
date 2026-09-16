19c9835bff6696647e7637aab37029e6bc6731f5
avinash-shinde-dev <avinash.shinde.dev@gmail.com>
2026-09-16 19:47:36 +0530

    feat: Product service added

--- src/main/java/com/shikavani/lld/vendingmachine/service/ProductService.java
+++ src/main/java/com/shikavani/lld/vendingmachine/service/ProductService.java
@@ -1,4 +1,51 @@
 package com.shikavani.lld.vendingmachine.service;
 
+import com.shikavani.lld.vendingmachine.exception.ProductNotFoundException;
+import com.shikavani.lld.vendingmachine.inventory.ProductInventory;
+import com.shikavani.lld.vendingmachine.model.payment.Price;
+import com.shikavani.lld.vendingmachine.model.Product;
+import com.shikavani.lld.vendingmachine.repository.ProductRepository;
+
 public class ProductService {
+
+    private final ProductRepository productRepository;
+    private  final ProductInventory productInventory;
+
+    public ProductService(ProductRepository productRepository, ProductInventory productInventory) {
+        this.productRepository = productRepository;
+        this.productInventory = productInventory;
+    }
+
+    public Product addProduct(Product product){
+       Product savedProduct =  this.productRepository.save(product);
+       this.productInventory.incrementQuantity(savedProduct.getId());
+       return savedProduct;
+    }
+
+    public Product addProductInStock(Product product, Integer quantity){
+        Product savedProduct =  this.productRepository.save(product);
+        restockProducts(product.getId(), quantity);
+        return savedProduct;
+    }
+
+    public void restockProducts(String productId, Integer quantity) {
+        this.productInventory.incrementByQuantity(productId, quantity);
+    }
+
+    public void removeProduct(String productId){
+        this.productRepository.delete(productId);
+        this.productInventory.removeProduct(productId);
+    }
+
+
+    public Product getProductById(String productId){
+        return this.productRepository.findById(productId).orElseThrow(() -> new ProductNotFoundException(String.format("Product: %s doesn't exist", productId)));
+    }
+
+    public Product updateProductPrice(String productId, Price newPrice){
+         Product product = this.getProductById(productId);
+        product.setPrice(newPrice);
+        return this.productRepository.save(product);
+    }
+
 }
