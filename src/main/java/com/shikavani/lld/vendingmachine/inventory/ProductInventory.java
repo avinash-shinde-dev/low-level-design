90df6c0b635addd7f2406f55f6b39d1b605e968b
avinash-shinde-dev <avinash.shinde.dev@gmail.com>
2026-09-16 19:45:43 +0530

    feat: cash and product inventory added

--- src/main/java/com/shikavani/lld/vendingmachine/inventory/ProductInventory.java
+++ src/main/java/com/shikavani/lld/vendingmachine/inventory/ProductInventory.java
@@ -1,4 +1,4 @@
-package com.shikavani.lld.vendingmachine.service;
+package com.shikavani.lld.vendingmachine.inventory;
 
 
 import com.shikavani.lld.vendingmachine.enums.StockStatus;
@@ -6,14 +6,20 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class InventoryService {
+public class ProductInventory {
+
     private final Map<String, Integer> inventoryMap;
-    public InventoryService() {
+
+    public ProductInventory() {
         inventoryMap = new ConcurrentHashMap<>();
     }
 
     public void incrementQuantity(String productId){
         inventoryMap.merge(productId,1, Integer::sum);
+    }
+
+    public void incrementByQuantity(String productId, Integer quantity){
+        inventoryMap.merge(productId,quantity, Integer::sum);
     }
 
     public void decrementQuantity(String productId){
@@ -23,8 +29,15 @@ public class InventoryService {
         );
     }
 
+    public void decrementByQuantity(String productId, Integer quantity){
+        inventoryMap.computeIfPresent(
+                productId,
+                (key, value) -> value > quantity ? value - quantity : null
+        );
+    }
+
     public boolean isProductAvailable(String productId){
-        return inventoryMap.get(productId) != null ?  true : false;
+        return inventoryMap.get(productId) != null;
     }
 
     public Set<String> getAllProductIds(){
@@ -34,9 +47,11 @@ public class InventoryService {
     public Integer getQuantity(String productId){
         return this.inventoryMap.get(productId);
     }
+
     public void removeProduct(String productId){
         this.inventoryMap.remove(productId);
     }
+
     public void displayInventory() {
         System.out.println("**** Inventory *****");
         this.inventoryMap.forEach((k, v) -> System.out.println(String.format("Product ID : %s Stock: %s ", k, v > 0 ? StockStatus.IN_STOCK: StockStatus.OUT_OF_STOCK)));
