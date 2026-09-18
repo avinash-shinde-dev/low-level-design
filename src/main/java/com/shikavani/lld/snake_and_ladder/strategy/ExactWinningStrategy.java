32956f17440e3e5fee2a5912df7ac2bcb4de59c0
avinash-shinde-dev <avinash.shinde.dev@gmail.com>
2026-09-18 19:39:30 +0530

    feat: Winning strategy added

---
+++ src/main/java/com/shikavani/lld/snake_and_ladder/strategy/ExactWinningStrategy.java
@@ -0,0 +1,11 @@
+package com.shikavani.lld.snake_and_ladder.strategy;
+
+import com.shikavani.lld.snake_and_ladder.model.Board;
+
+public class ExactWinningStrategy implements WinningStrategy{
+
+    @Override
+    public boolean win(Board board, int newPosition) {
+        return board.size() == newPosition;
+    }
+}
---
+++ src/main/java/com/shikavani/lld/snake_and_ladder/strategy/FirstOneToCrossWinningStrategy.java
@@ -0,0 +1,10 @@
+package com.shikavani.lld.snake_and_ladder.strategy;
+
+import com.shikavani.lld.snake_and_ladder.model.Board;
+
+public class FirstOneToCrossWinningStrategy implements WinningStrategy{
+    @Override
+    public boolean win(Board board, int newPosition) {
+        return newPosition > board.size();
+    }
+}
