16b3c4ea6507ff123d4a28cc7b3f273468db074b
avinash-shinde-dev <avinash.shinde.dev@gmail.com>
2026-09-22 22:39:56 +0530

    feat: PaymentService added

--- src/main/java/com/shikavani/lld/vendingmachine/service/PaymentService.java
+++ src/main/java/com/shikavani/lld/vendingmachine/service/PaymentService.java
@@ -1,4 +1,20 @@
 package com.shikavani.lld.vendingmachine.service;
 
+import com.shikavani.lld.vendingmachine.enums.PaymentType;
+import com.shikavani.lld.vendingmachine.model.transaction.PaymentOutcome;
+import com.shikavani.lld.vendingmachine.model.transaction.PaymentRequest;
+import com.shikavani.lld.vendingmachine.registry.StrategyRegistry;
+import com.shikavani.lld.vendingmachine.strategy.payment.PaymentStrategy;
+
 public class PaymentService {
+    private final StrategyRegistry<PaymentType, PaymentStrategy> paymentStrategies;
+
+    public PaymentService(StrategyRegistry<PaymentType, PaymentStrategy> paymentStrategies) {
+        this.paymentStrategies = paymentStrategies;
+    }
+
+    public PaymentOutcome pay(PaymentRequest paymentRequest){
+        PaymentStrategy paymentStrategy = paymentStrategies.getOrThrow(paymentRequest.paymentTender().type());
+        return paymentStrategy.pay(paymentRequest);
+    }
 }
